cc1e41b5421a42193fb9563388a80b050d56decd
yermyerm <songkyuu@gmail.com>
2023-09-19 23:59:19 +0900

    3.4강까지

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("<h1>Login here.</h1>");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
