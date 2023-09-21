89fedd897e212e9acaacd852632fb302b835b60f
yermyerm <songkyuu@gmail.com>
2023-09-21 23:51:14 +0900

    3.11까지

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
-const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
