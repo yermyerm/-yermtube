b040bcfc3718fd72b28c483f01b2d0b3acf0924e
yermyerm <songkyuu@gmail.com>
2023-09-25 22:24:24 +0900

    #5.9까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,40 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2mins ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 3,
+      comments: 0,
+      createdAt: "3days ago",
+      views: 1000,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 2,
+      comments: 4,
+      createdAt: "10mins ago",
+      views: 855,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => res.send("Search Videos");
-
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) =>
+  res.render("watch", {
+    pageTitle: `Video #${req.params.id}`,
+    videoId: req.params.id,
+  });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("Delete Video");
