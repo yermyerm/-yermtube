6a2e1221fc894708ced015612b1376b720f2e7e3
yermyerm <songkyuu@gmail.com>
2023-09-30 23:40:26 +0900

    #6.1까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,39 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2mins ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 3,
+    comments: 0,
+    createdAt: "3days ago",
+    views: 1000,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 2,
+    comments: 4,
+    createdAt: "10mins ago",
+    views: 855,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2mins ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 3,
-      comments: 0,
-      createdAt: "3days ago",
-      views: 1000,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 2,
-      comments: 4,
-      createdAt: "10mins ago",
-      views: 855,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("Search Videos");
-export const see = (req, res) =>
-  res.render("watch", {
-    pageTitle: `Video #${req.params.id}`,
-    videoId: req.params.id,
-  });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+};
 export const edit = (req, res) =>
   res.render("edit", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { see, edit, deleteVideo, upload } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  deleteVideo,
+  upload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
 
