d3f2a5bd1f0be1d01c40183816e6264d2a351a1c
yermyerm <songkyuu@gmail.com>
2023-10-02 17:09:35 +0900

    #6.8까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,3 +44,19 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload a Video" });
+};
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array.
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
