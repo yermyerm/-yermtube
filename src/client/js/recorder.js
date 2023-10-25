69dcc66b3d29b631ae86094a1527deb58ded9ffb
yermyerm <songkyuu@gmail.com>
2023-10-26 00:05:09 +0900

    #13.2 5:00까지

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,24 @@
 const startBtn = document.getElementById("startBtn");
-
-const handleStart = async () => {
+const video = document.getElementById("preview");
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+};
+const init = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
